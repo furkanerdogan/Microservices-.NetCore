ac308c8e3cd0f0d65aab26bb75f8f1c86e6b7c61
furkanerdogan1 <furkanerdogannnn@gmail.com>
2023-08-06 17:34:23 +0300

    photostock api created

--- Microservices/Shared/Course.Shared/Dtos/ErrorDto.cs
+++ Microservices/Shared/Course.Shared/Dtos/ErrorDto.cs
@@ -1,14 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Course.Shared.Dtos
 {
     public class ErrorDto
     {
         public List<string> Errors { get; set; }
-
     }
 }
